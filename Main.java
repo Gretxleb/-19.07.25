ffcc5d1f802c179d1bda2a92a2c52d39224b1efe
Gretxleb <96791484+Gretxleb@users.noreply.github.com>
2025-07-20 15:38:34 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,19 @@ class Main {
         System.out.println("День добрый!");
 
         System.out.print("Введите цену товара (в руб.):");
-        int x = scanner.nextInt();
-
-        System.out.print("Введите вес товара (в кг.):");
         int price = scanner.nextInt();
 
-        double dutyAmount = (x / 100) + (price * 100);
+        System.out.print("Введите вес товара (в кг.):");
+        int kg = scanner.nextInt();
+
+        double dutyAmount = calculateDuty(price, kg);
 
         System.out.print("Размер пошлины (в руб.) составит: " + (int)dutyAmount);
 
+        scanner.close();
+    }
+
+    public static double calculateDuty(int price, int kg) {
+        return (double) (price / 100) + (kg * 100);
     }
 }
